34980f43e865750aeb3fd3c789478977dd89598f
bharat prasad <bharat.prasad@accenture.com>
2025-09-18 14:00:12 +0530

    added production url to the allowed origins.

--- src/main/java/com/utilityzone/security/SecurityConfig.java
+++ src/main/java/com/utilityzone/security/SecurityConfig.java
@@ -86,6 +86,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOrigin("https://utilityzone.in");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
